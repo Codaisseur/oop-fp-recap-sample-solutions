8b84ef763f2548b13a65065befd943d3961d368e
Arien Kock <arien@positor.nl>
2019-01-14 11:40:16 +0100

    Implement "Build a farm"

--- index.js
+++ index.js
@@ -2,23 +2,6 @@
 class Farm {
   constructor(name) {
     this.name = name
-    this.crops = []
-    this.animals = []
-  }
-
-  addCrop(crop) {
-    this.crops.push(crop)
-  }
-
-  addAnimal(animal) {
-      this.animals.push(animal)
-  }
-
-  calculateIncome() {
-    return this.crops
-      .map(crop => crop.getYieldInEuros())
-      .concat(this.animals.map(a => a.getValueInEuros()))
-      .reduce((a,b) => a + b, 0)
   }
 }
 module.exports.Farm = Farm
