9c4da33cb4116eddd71dfe0dbe0a95d7b12bf4ce
Arien Kock <arien@positor.nl>
2019-01-14 12:45:13 +0100

    Implement "Full farm"

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ class Farm {
   constructor(name) {
     this.name = name
     this.crops = []
+    this.animals = []
   }
 
   addCrop(crop) {
     this.crops.push(crop)
   }
 
+  addAnimal(animal) {
+    this.animals.push(animal)
+  }
+
   calculateIncome() {
     return this.crops
       .map(crop => crop.getYieldInEuros())
+      .concat(this.animals.map(animal => animal.getValueInEuros()))
       .reduce((a, b) => a + b, 0)
+  }
+
+  printReport() {
+    console.log(`
+ ----------------------
+ - Farm: FARM NAME    -
+ - No. of crops: ${this.crops.length}   -
+ - No. of animals: ${this.animals.length} -
+ - Total income: ${this.calculateIncome()}€ -
+ ----------------------
+    `)
   }
 }
 module.exports.Farm = Farm
