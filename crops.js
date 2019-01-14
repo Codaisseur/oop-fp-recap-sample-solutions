2dc5171e02deaf9634f70cd0b39d9e6fb15c6474
Arien Kock <arien@positor.nl>
2019-01-14 12:15:02 +0100

    Implement "Crops" 3/3 yield in KG plus costs

--- crops.js
+++ crops.js
@@ -3,16 +3,31 @@ class Crop {
     constructor(acres) {
         this.acres = acres
     }
+    getYieldInEuros() {
+        return this.getYieldInKg() * this.price
+    }
+    getCosts() {
+        return this.acres * this.costs
+    }
 }
 
 class Wheat extends Crop {
+    constructor(acres) {
+        super(acres)
+        this.price = 1.5
+        this.costs = 340
+    }
     getYieldInKg() {
         return Math.pow(this.acres * 1.5, 1.3)
     }
 }
 
-// crops.js
 class Sugarcane extends Crop {
+    constructor(acres) {
+        super(acres)
+        this.price = 2.0
+        this.costs = 166
+    }
     getYieldInKg() {
         return Math.pow(this.acres * 2.6, 1.1)
     }
